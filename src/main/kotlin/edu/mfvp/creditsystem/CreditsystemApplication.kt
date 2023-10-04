09db3c5562321c53449d9cc45dc141b850670ff1
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2023-10-04 11:10:47 -0300

    first commit

---
+++ src/main/kotlin/edu/mfvp/creditsystem/CreditsystemApplication.kt
@@ -0,0 +1,11 @@
+package edu.mfvp.creditsystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditsystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditsystemApplication>(*args)
+}
---
+++ src/test/kotlin/edu/mfvp/creditsystem/CreditsystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.mfvp.creditsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditsystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
