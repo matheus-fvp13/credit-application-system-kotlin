16329d60e8f40f14774bc83d326bfbdcd5c35dac
Matheus Fernando Vieira Pinto <matheus.fvp@outlook.com>
2023-10-04 14:51:53 -0300

    add JPA annotations in the model classes

--- src/main/kotlin/edu/mfvp/creditsystem/domain/model/Credit.kt
+++ src/main/kotlin/edu/mfvp/creditsystem/domain/model/Credit.kt
@@ -1,16 +1,18 @@
 package edu.mfvp.creditsystem.domain.model
 
 import edu.mfvp.creditsystem.domain.enumeration.Status
+import jakarta.persistence.*
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
 
+@Entity(name = "tb_credit")
 data class Credit(
-    val creditCode: UUID = UUID.randomUUID(),
-    val creditValue: BigDecimal = BigDecimal.ZERO,
-    val dayFirstInstallment: LocalDate,
-    val numberOfInstallments: Int = 0,
-    val status: Status = Status.IN_PROGRESS,
-    val customer: Customer? = null,
-    val id: Long? = null
+    @Column(nullable = false, unique = true) val creditCode: UUID = UUID.randomUUID(),
+    @Column(nullable = false) val creditValue: BigDecimal = BigDecimal.ZERO,
+    @Column(nullable = false) val dayFirstInstallment: LocalDate,
+    @Column(nullable = false) val numberOfInstallments: Int = 0,
+    @Enumerated val status: Status = Status.IN_PROGRESS,
+    @ManyToOne @JoinColumn(name = "customer_id_fk") val customer: Customer? = null,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
--- src/main/kotlin/edu/mfvp/creditsystem/domain/model/Customer.kt
+++ src/main/kotlin/edu/mfvp/creditsystem/domain/model/Customer.kt
@@ -1,12 +1,16 @@
 package edu.mfvp.creditsystem.domain.model
 
+import jakarta.persistence.*
+
+@Entity(name = "tb_customer")
 data class Customer(
-    var firstName: String = "",
-    var lastName: String = "",
-    val cpf: String,
-    var email: String = "",
-    var password: String = "",
-    var address: Address = Address(),
-    var credits: List<Credit> = mutableListOf(),
-    val id: Long? = null
+    @Column(nullable = false) var firstName: String = "",
+    @Column(nullable = false) var lastName: String = "",
+    @Column(nullable = false, unique = true) val cpf: String,
+    @Column(nullable = false, unique = true) var email: String = "",
+    @Column(nullable = false) var password: String = "",
+    @Column(nullable = false) @Embedded var address: Address = Address(),
+    @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY,
+        cascade = [CascadeType.REMOVE], mappedBy = "customer") var credits: List<Credit> = mutableListOf(),
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
